a6eafadace2cc4c8e549ccf046ae8553274d54dc
drocpdp <davidreynon@gmail.com>
2023-08-04 22:38:23 +0900

    session object initial work. Storing name, description, price, uuid of item

--- ecommerce/mysite/items/urls.py
+++ ecommerce/mysite/items/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("clear_cart/", views.clear_cart, name="clear_cart"),
     path("<int:item_id>/", views.item_request, name="index"),
     path("additem/<uuid:item_uuid>/", views.add_item_to_cart, name="additem")
 
--- ecommerce/mysite/items/views.py
+++ ecommerce/mysite/items/views.py
@@ -13,8 +13,30 @@ def add_item_to_cart(request, item_uuid=-1):
     print(request)
     post_obj = get_object_or_404(ItemForSale, item_uuid=item_uuid)
     print(post_obj.item_name, post_obj.item_description, post_obj.item_price)
+
+    cart_item = {
+        'item_uuid': post_obj.item_uuid,
+        'item_name': post_obj.item_name,
+        'item_description': post_obj.item_description,
+        'item_price': str(post_obj.item_price)
+    }
+
+    if 'cart_items' not in request.session:
+        request.session['cart_items'] = [cart_item]
+    else:
+        cart_items = request.session['cart_items']
+        cart_items.append(cart_item)
+        request.session['cart_items'] = cart_items
+    
+    print(request.session.items())
     return HttpResponse("Items added to cart. ID=" + 
                         ", " + str(post_obj.item_name) + 
                         ", " + str(post_obj.item_description) + 
                         ", " + str(post_obj.item_price) + 
-                        ", " + str(post_obj.item_uuid))
+                        ", " + str(post_obj.item_uuid) + '<br><a href="/">Back to Home</a>' + '<br><a href="/items/clear_cart/">Clear Cart</a>')
+
+def clear_cart(request):
+    if 'cart_items' in request.session:
+        del request.session['cart_items']
+    
+    return HttpResponse("Cart has been cleared. <a href='/'>Back to home</a>")
